0c3a9d3c8e5ca28580d109ea80bd453531cf1b2a
code-sensei <tsowababangida@gmail.com>
2025-06-09 20:49:42 +0100

    ui: improve property card feature spacing

--- app/components/PropertyCard.tsx
+++ app/components/PropertyCard.tsx
@@ -60,7 +60,7 @@ const PropertyCard = ({ property }: { property: Property }): React.ReactElement 
           <p className="text-gray-500 text-sm -mt-3">Per Annum</p>
           
           {/* Features Row */}
-          <div className="flex items-center gap-6 mt-2">
+          <div className="flex items-center justify-between mt-2">
             {/* Bedroom */}
             <div className="flex items-center">
               <svg className="w-5 h-5 mr-1 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
